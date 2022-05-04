f3c49fdcb9f8a64f0a50c2b76cba659b16196b4b
Oscar Moll <orm@csail.mit.edu>
2022-05-04 14:20:57 -0400

    remove dup

--- frameserver/index.py
+++ frameserver/index.py
@@ -74,41 +74,7 @@ import skvideo.io
 import sys
 import json
 
-_rotation_map = {'90':PIL.Image.Transpose.ROTATE_90,
-                '270':PIL.Image.Transpose.ROTATE_270,
-                '180':PIL.Image.Transpose.ROTATE_180,
-                '0':None,
-                }
 
-class FFProbe:
-    def __init__(self, ffprobe):
-        self.ffprobe = ffprobe if ffprobe else {}
-
-    @staticmethod
-    def get(path):
-        ffprobe = skvideo.io.ffprobe(path)
-        return FFProbe(ffprobe)
-
-    def get_rotation(self):
-        rot =  (self.ffprobe.get('video', {})
-            .get('side_data_list', {})
-            .get('side_data', {})
-            .get('@rotation', None))
-
-        return _rotation_map.get(rot, None)
-
-    def get_frame_size(self):
-        x = self.ffprobe
-        width = int(x.get('video',{}).get('@width', None))
-        height = int(x.get('video',{}).get('@height', None))
-
-        rot = self.get_rotation()
-        if rot in [PIL.Image.Transpose.ROTATE_90, PIL.Image.Transpose.ROTATE_270]:
-            tmp = width
-            width = height
-            height = tmp
-        
-        return (width, height)
     
 class VideoFrameIndex:
     def __init__(self, packet_df, frame_df):
