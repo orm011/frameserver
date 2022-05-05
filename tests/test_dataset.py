4143a9d7000dcf8a94c2322f0e2c854265368c70
Oscar Moll <orm@csail.mit.edu>
2022-05-05 12:20:30 -0400

    add keyframe test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -37,3 +37,28 @@ def test_dataloader_all(path, num_workers):
     assert pts0.shape == pts1.shape
     assert (pts0 == pts1).all()
     assert (hashes0 == hashes1).all()
+
+@pytest.mark.parametrize('path,num_workers', list(product(videos, [11])))
+def test_dataloader_keyframe(path, num_workers):
+    vd = TorchVideoDataset(path, format='tensor', keyframes_only=True, image_tx=None)
+
+    def process_batches(batches):
+        pts = torch.cat([b['pts'] for b in batches])
+        pts, indices = torch.sort(pts)
+        tensors = np.array(torch.cat([b['image'] for b in batches]))
+        tensors = tensors[indices]
+        return pts.numpy(), tensors
+
+    batches_serial = []
+    for batch in DataLoader(vd, batch_size=13):
+        batches_serial.append(batch)
+
+    batches_parallel = []
+    for batch in DataLoader(vd, num_workers=num_workers, batch_size=13):
+        batches_parallel.append(batch)
+
+    pts0,tensors0 = process_batches(batches_serial)
+    pts1,tensors1 = process_batches(batches_parallel)
+    assert pts0.shape == pts1.shape
+    assert (pts0 == pts1).all()
+    assert (tensors0 == tensors1).all()
